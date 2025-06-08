eddd63e2e8dd165d4f970224da3ed5779d9c73e1
Aashuu4 <90998503+Aashuu4@users.noreply.github.com>
2025-06-08 11:00:14 +0530

    Update assignment14_9.py

--- Assignment14/assignment14_9.py
+++ Assignment14/assignment14_9.py
@@ -1,6 +1,24 @@
 class Product:
     
-    def __init__(self,a,b):
-        self.name=a
-        self.price=b
+   
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __eq__(self, other):
+        if( self.price == other.price):
+            print("Price of both the products is same")
+        else:
+            print("Price of both the products is not  same")
+            
         
+
+def main():
+    obj1 = Product("Apple", 10)
+    obj2 = Product("Orange", 12)
+    Product.__eq__(obj1,obj2)
+    
+if __name__=="__main__":
+    main()
+
+
